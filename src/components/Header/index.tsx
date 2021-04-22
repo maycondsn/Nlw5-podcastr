ad1a6daff361bc50f845d05ea2f843ae4bd3b608
maycondsn <maycondsn@gmail.com>
2021-04-21 22:53:47 -0400

    3º Day

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <img src="logo.svg" alt="logo"/>
+      <img src="/logo.svg" alt="logo"/>
       <p>O melhor para você ouvir, sempre</p>
 
       <span>{currentDate}</span>
